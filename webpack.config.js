ef6d4b0672d27412093bc1be22d66c13d02fbb10
Claude Betancourt <cbetancourt@gmail.com>
2018-02-11 19:06:01 -0500

    Added NODE_ENV=production

--- webpack.config.js
+++ webpack.config.js
@@ -65,6 +65,9 @@ module.exports = {
 	].concat(
 		isProduction
 			? [
+				new webpack.DefinePlugin({
+					'process.env.NODE_ENV': '"production"'
+				}),
 				new webpack.optimize.UglifyJsPlugin()
 			]
 			: [
